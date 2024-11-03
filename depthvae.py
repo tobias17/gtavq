b83dae366c1f0d581b874f8dc83ffd35b08ae05f
Tobias Fischer <tobiasfischer17@gmail.com>
2024-11-03 13:00:15 -0500

    saving of model weights

--- depthvae.py
+++ depthvae.py
@@ -1,6 +1,6 @@
 from tinygrad import Tensor, TinyJit, dtypes
 from tinygrad.nn.optim import AdamW
-from tinygrad.nn.state import get_parameters
+from tinygrad.nn.state import get_parameters, safe_save, get_state_dict
 from dataclasses import dataclass
 from vqvae import Encoder, Decoder
 import matplotlib.pyplot as plt
@@ -61,6 +61,15 @@ def get_next_pack_path():
       for filename in os.listdir(f"{ROOT}/{splitdir}"):
          yield f"{ROOT}/{splitdir}/{filename}"
 
+def underscore_number(value:int) -> str:
+   text = ""
+   for magnitude in [1_000_000, 1_000]:
+      if value >= magnitude:
+         upper, value = value // magnitude, value % magnitude
+         text += f"{upper}_" if len(text) == 0 else f"{upper:03d}_"
+   text += f"{value}" if len(text) == 0 else f"{value:03d}"
+   return text
+
 def train():
    Tensor.training = True
    Tensor.manual_seed(42)
@@ -69,10 +78,10 @@ def train():
 
    TRAIN_DTYPE = dtypes.float32
    GLOBAL_BS = 16
-   PLOT_EVERY = 50
-   SAVE_EVERY = 500
+   PLOT_EVERY = 100
+   SAVE_EVERY = 1000
 
-   LEARNING_RATE = 2**-18
+   LEARNING_RATE = 2**-20
    optim = AdamW(get_parameters(model), lr=LEARNING_RATE)
 
    depthpack_getter = get_next_pack_path()
@@ -100,6 +109,7 @@ def train():
    data = None
    step_i = 0
    losses = []
+   prev_weights = None
 
    s_t = time.perf_counter()
    while True:
@@ -141,6 +151,13 @@ def train():
          fig = plt.gcf()
          fig.set_size_inches(18, 10)
          plt.savefig(save_path("graph_loss.png"))
+
+      if step_i % SAVE_EVERY == 0:
+         curr_weights = save_path(f"weights_{underscore_number(step_i)}.st")
+         safe_save(get_state_dict(model), curr_weights)
+         if prev_weights is not None:
+            os.remove(prev_weights)
+         prev_weights = curr_weights
 
       e_t = time.perf_counter()
       print(f"{step_i:04d}, {(e_t-s_t)*1000:.0f} ms step ({(l_t-s_t)*1000:.0f} load, {(e_t-l_t)*1000:.0f} run), loss: {losses[-1]:.3f}")
