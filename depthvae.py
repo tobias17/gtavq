b96dad5f82805ed59cfa01e2670c72d11037fd35
Tobias Fischer <tobiasfischer17@gmail.com>
2024-11-05 23:08:47 -0500

    info file store and load

--- depthvae.py
+++ depthvae.py
@@ -1,13 +1,14 @@
 from tinygrad import Tensor, TinyJit, dtypes, Device
 from tinygrad.helpers import Context, BEAM
 from tinygrad.nn.optim import AdamW
-from tinygrad.nn.state import get_parameters, safe_save, get_state_dict
-from dataclasses import dataclass
+from tinygrad.nn.state import get_parameters, get_state_dict, load_state_dict, safe_save, safe_load
+from dataclasses import dataclass, asdict
 from vqvae import Encoder, Decoder
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-import os, time, datetime, random
+import os, time, datetime, random, json, argparse
+from typing import List, Dict
 
 # @dataclass
 # class CompressorConfig:
@@ -58,8 +59,8 @@ class VQModel:
       self.dec.quantize = self.enc.quantize
 
 __input_dims = -1
-__dataset_cache = None # type: ignore
-def get_random_batch(batch_size:int):
+__dataset_cache: List | None = None
+def get_random_batch(batch_size:int) -> np.ndarray:
    global __dataset_cache, __input_dims
    if __dataset_cache is None:
       target_shape = None
@@ -90,11 +91,18 @@ def underscore_number(value:int) -> str:
    text += f"{value}" if len(text) == 0 else f"{value:03d}"
    return text
 
+def seed_all(seed:int):
+   Tensor.manual_seed(seed)
+   np.random.seed(seed)
+   random.seed(seed)
+
 def train():
+   parser = argparse.ArgumentParser()
+   parser.add_argument('-r', '--restore', type=str)
+   args = parser.parse_args()
+
    Tensor.training = True
-   Tensor.manual_seed(42)
-   np.random.seed(42)
-   random.seed(42)
+   seed_all(42)
 
    TRAIN_DTYPE = dtypes.float32
    BEAM_VALUE  = BEAM.value
@@ -104,15 +112,35 @@ def train():
    DEVICE_BS = 32
    GLOBAL_BS = DEVICE_BS * len(GPUS)
 
-   model = VQModel()
-   params = set(get_parameters(model))
-   for w in params:
-      w.replace(w.shard(GPUS).cast(TRAIN_DTYPE)).realize()
-
    AVG_EVERY  = 100
    PLOT_EVERY = 500
    EVAL_EVERY = 5000
    SAVE_EVERY = 5000
+
+   model = VQModel()
+   params = set(get_parameters(model))
+
+   @dataclass
+   class TrainInfo:
+      step_i = 0
+      losses = []
+      prev_weights = None
+      def to_json(self): return asdict(self)
+      @staticmethod
+      def from_json(data:Dict) -> 'TrainInfo': return TrainInfo(**data)
+   
+   if args.restore:
+      assert os.path.exists(args.restore), f"failed to find restore root, searched for {args.restore}"
+      data_filepath = os.path.join(args.restore, "data.json")
+      assert os.path.exists(data_filepath), f"failed to find data.json file in restore root, searched in {args.restore}"
+      with open(data_filepath, "r") as f:
+         info = TrainInfo.from_json(json.load(f))
+      load_state_dict(model, safe_load(info.prev_weights))
+   else:
+      info = TrainInfo()
+
+   for w in params:
+      w.replace(w.shard(GPUS).cast(TRAIN_DTYPE)).realize()
 
    LEARNING_RATE = 2**-22
    optim = AdamW(params, lr=LEARNING_RATE)
@@ -139,14 +167,12 @@ def train():
 
    eval_inputs = get_random_batch(len(GPUS))
    eval_input = Tensor(eval_inputs).shard(GPUS)
-
-   step_i = 0
-   all_losses = []
    curr_losses = []
-   prev_weights = None
 
    s_t = time.perf_counter()
    while True:
+      seed_all(info.step_i)
+
       init_x = Tensor.cat(get_random_batch()).shard(GPUS, axis=0).realize()
       assert init_x.shape[0] == GLOBAL_BS, f"{init_x.shape[0]=}, expected BS={GLOBAL_BS}"
       l_t = time.perf_counter()
@@ -154,30 +180,32 @@ def train():
       with Context(BEAM=BEAM_VALUE):
          loss = train_step(init_x)
 
-      step_i += 1
+      info.step_i += 1
       curr_losses.append(loss.item())
 
-      if step_i % AVG_EVERY == 0:
-         all_losses += (sum(curr_losses) / len(curr_losses))
+      if info.step_i % AVG_EVERY == 0:
+         info.losses += (sum(curr_losses) / len(curr_losses))
          curr_losses = []
 
-      if step_i % PLOT_EVERY == 0:
+      if info.step_i % PLOT_EVERY == 0:
          plt.clf()
-         plt.plot(np.arange(1, len(all_losses)+1)*GLOBAL_BS, all_losses)
+         plt.plot(np.arange(1, len(info.losses)+1)*GLOBAL_BS, info.losses)
          plt.ylim((0,None))
          plt.title("Loss")
          fig = plt.gcf()
          fig.set_size_inches(18, 10)
          plt.savefig(save_path("graph_loss.png"))
 
-      if step_i % SAVE_EVERY == 0:
-         curr_weights = save_path(f"weights_{underscore_number(step_i)}.st")
+      if info.step_i % SAVE_EVERY == 0:
+         curr_weights = save_path(f"weights_{underscore_number(info.step_i)}.st")
          safe_save(get_state_dict(model), curr_weights)
-         if prev_weights is not None:
-            os.remove(prev_weights)
-         prev_weights = curr_weights
+         if info.prev_weights is not None:
+            os.remove(info.prev_weights)
+         info.prev_weights = curr_weights
+         with open(save_path("data.json"), "w") as f:
+            json.dump(info.to_json(), f)
 
-      if step_i % EVAL_EVERY == 0:
+      if info.step_i % EVAL_EVERY == 0:
          inputs_dirpath = save_path("evals", "input_0.png")
          if not os.path.exists(inputs_dirpath):
             for i in range(len(GPUS)):
@@ -187,10 +215,10 @@ def train():
          print(pred_x.shape)
          for i in range(len(GPUS)):
             img = pred_x[i].reshape(*pred_x[i].shape[-2:])
-            Image.fromarray(img).save(save_path("evals", underscore_number(step_i), f"output_{i}.png"))
+            Image.fromarray(img).save(save_path("evals", underscore_number(info.step_i), f"output_{i}.png"))
 
       e_t = time.perf_counter()
-      print(f"{step_i:04d}, {(e_t-s_t)*1000:.0f} ms step ({(l_t-s_t)*1000:.0f} load, {(e_t-l_t)*1000:.0f} run), loss: {loss.item():.3f}")
+      print(f"{info.step_i:04d}, {(e_t-s_t)*1000:.0f} ms step ({(l_t-s_t)*1000:.0f} load, {(e_t-l_t)*1000:.0f} run), loss: {loss.item():.3f}")
       s_t = e_t
 
 if __name__ == "__main__":
